6ddc59a4c418f020dd57cd68350305805ad86652
Kaiyu Wang <cathywky123@gmail.com>
2020-10-04 21:33:25 -0400

    fix lint error

--- app/pods/products/index/controller.js
+++ app/pods/products/index/controller.js
@@ -1,9 +1,10 @@
 import Controller from '@ember/controller';
+import Ember from 'ember';
 import { alias } from '@ember/object/computed';
 
 export default Controller.extend({
   publisher: alias('model.publisher'),
-  products: Ember.computed.alias('model.products'),
+  products: alias('model.products'),
   actions: {
     clickButton(element) {
       Ember.Logger.info('A button has been clicked: ', element)
--- app/pods/products/show/controller.js
+++ app/pods/products/show/controller.js
@@ -1,11 +1,12 @@
 import Controller from '@ember/controller';
+import Ember from 'ember';
 import { alias } from '@ember/object/computed';
 
 export default Controller.extend({
   publisher: alias('model.publisher'),
   product: alias('model.product'),
   productData: alias('product.data'),
-  products: Ember.computed.alias('model.products'),
+  products: alias('model.products'),
   actions: {
     logClick(element) {
       Ember.Logger.info('An element has been clicked: ', element)
